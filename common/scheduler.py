b8d2efa79b55e41d405c14798c41ff88cedd17a3
toxicaker <toxicaker@liangjiateng.cn>
2018-06-24 01:56:41 +0800

    change scheduler

--- common/scheduler.py
+++ common/scheduler.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 '任务调度器'
+import importlib
+
+from apscheduler.triggers.cron import CronTrigger
+
 from model.job import Job
 
 __author__ = 'Jiateng Liang'
@@ -20,6 +24,11 @@ class Scheduler(object):
         if not job.job_id in self._all_jobs:
             self._all_jobs[job.job_id] = job
         self._all_jobs[job.job_id].status = Job.Status.RUNNING.value
+        # 动态导入脚本
+        script = importlib.import_module(job.job_id)
+
+        self._scheduler.add_job(script.run, CronTrigger.from_crontab(job.cron), id=job.job_id,
+                                start_date=job.start_date, end_date=job.end_date)
 
     def suspend_job(self, job):
         if job.job_id in self._all_jobs:
@@ -32,5 +41,6 @@ class Scheduler(object):
     def remove_job(self, job):
         if job.job_id in self._all_jobs:
             del self._all_jobs[job.job_id]
+
 
 scheduler = Scheduler()
